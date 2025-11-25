6c77d8906f3ab712a7641e4709ce137fc49add07
Satya Douchet <sdouchet@islstudent.ch>
2025-11-25 12:40:05 +0100

    Basic interface Created

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+import re
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'
 
 @app.route('/')
 def index():
@@ -8,13 +10,23 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    name = request.register['name']
-    email = request.register['email']
-    return f"Thank you, {name}. Your email address is {email}."
+    name = request.form['first_name']
+    last_name = request.form['last_name']
+    birth_date = request.form['birth_date']
+    email = request.form['email']
+    if not name or not last_name or not birth_date or not email:
+        flash('all fields are required to be filled in!')
+        return redirect(url_for('register'))
+    else:
+        print(f"Thank you {name}, your contact information has been saved!.")
+        return f"Thank you {name}, your contact information has been saved!."
 
 @app.route('/register')
 def register():
     return render_template("register.html")
+
+
+
 
 
 if __name__ == '__main__':
