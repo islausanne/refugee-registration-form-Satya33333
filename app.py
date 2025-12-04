eba136f7614688f8fb303222ecd27bd045074d47
Satya Douchet <sdouchet@islstudent.ch>
2025-12-04 14:51:17 +0100

    Thursday of week 4

--- app.py
+++ app.py
@@ -18,19 +18,22 @@ def submit():
     phone_number = request.form['phone_number']
     phone_list = request.form['phone']
     medical_assistance = request.form['medical_assistance']
+    medical_box = request.form['medical_box']
+    extra_box = request.form['extra_box']
 
     if os.path.exists('registrations.json'):
         with open('registrations.json', 'r') as file:
             data = json.load(file)
+
     else:
         data = []
 
-    data.append({'name': name, 'last_name': last_name, 'birth_date': birth_date, 'email': email, 'phone_number': phone_number, 'phone_list': phone_list, 'medical_assistance': medical_assistance})
+    data.append({'name': name, 'last_name': last_name, 'birth_date': birth_date, 'email': email, 'phone_number': phone_number, 'phone_list': phone_list, 'medical_assistance': medical_assistance, 'medical_box': medical_box, 'extra_box': extra_box})
     with open('registrations.json', 'w') as file:
         json.dump(data, file, indent=2)
     return redirect(url_for('index'))
 
-    if not name or not last_name or not birth_date or not email or not phone_number or not phone_list or not medical_assistance:
+    if not name or not last_name or not birth_date or not email or not phone_number or not phone_list:
         flash('all fields are required to be filled in!')
         return redirect(url_for('register'))
     else:
@@ -48,9 +51,13 @@ def register():
 
 @app.route('/view')
 def view_registrations():
-    with open('registrations.json','r') as file:
-        data = json.load(file)
-    return render_template('view.html', registrations=data)
+    if os.path.exists('view.html'):
+        data = [...]
+        return render_template('view.html', registrations=data)
+    else:
+        data = []
+        return render_template('view.html', registrations=data)
+
 
 
 if __name__ == '__main__':
